faefc02d26ca3567540e73f5ea122e8c102f9f4b
mtoutcalt <mtoutcalt@gmail.com>
2018-04-09 23:07:35 -0400

    add audio to phases of game and color step of deck builder

--- magic-manager/src/app/deck-builder-colors/deck-builder-colors.component.ts
+++ magic-manager/src/app/deck-builder-colors/deck-builder-colors.component.ts
@@ -10,6 +10,31 @@ export class DeckBuilderColorsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    document.getElementById('whiteChosen').addEventListener('click',
+      function (event) {
+        alert("WHITE");
+    });
+
+    document.getElementById('blueChosen').addEventListener('click',
+      function (event) {
+        alert("BLUE");
+    });
+
+    document.getElementById('greenChosen').addEventListener('click',
+      function (event) {
+        alert("GREEN");
+    });
+
+    document.getElementById('redChosen').addEventListener('click',
+      function (event) {
+        alert("RED");
+    });
+
+    document.getElementById('blackChosen').addEventListener('click',
+      function (event) {
+        alert("BLACK");
+    });
+
   }
 
 }
