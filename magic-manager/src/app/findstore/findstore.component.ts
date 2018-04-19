ab2cdeb7dae04b0a3c142749b33b28803ebeccf8
mtoutcalt <mtoutcalt@gmail.com>
2018-04-18 21:29:41 -0400

    finished find store by native geolocation and location input textbox

--- magic-manager/src/app/findstore/findstore.component.ts
+++ magic-manager/src/app/findstore/findstore.component.ts
@@ -21,9 +21,39 @@ export class FindstoreComponent implements OnInit {
   constructor(private findStoreService: FindStoreService) { }
 
   ngOnInit() {
+    var coordinatePromise = this.getLocalPosition();
+    coordinatePromise.then( (coordinates: any) => {
+      console.log(coordinates.latitude);
+      console.log(coordinates.longitude);
+      this.getClosestGamingStore(coordinates.latitude, coordinates.longitude);
+    });
 
+  } //OnInit
+
+   getLocalPosition() {
+      return new Promise(function(resolve, reject) {
+
+        var options = {
+          enableHighAccuracy: true,
+          timeout: 5000,
+          maximumAge: 0
+        };
+
+        function success (pos) {
+            resolve(pos.coords);
+        }
+
+        function fail(error){
+            console.log('geolocation failed');
+            resolve();
+        }
+
+        navigator.geolocation.getCurrentPosition(success, fail, options);
+      });
+  }
+
+  getClosestGamingStore(lat, long) {
     let mapElement = document.getElementById('map');
-    let otherMapElement = document.getElementById('othermap');
     let panelElement = document.getElementById('panel')
 
     let directionsService = new google.maps.DirectionsService();
@@ -37,38 +67,10 @@ export class FindstoreComponent implements OnInit {
     directionsDisplay.setMap(map);
     directionsDisplay.setPanel(panelElement);
 
-    var options = {
-      enableHighAccuracy: true,
-      timeout: 5000,
-      maximumAge: 0
-    };
-
-    function success(pos) {
-      var crd = pos.coords;
-      console.log('Your current position is:');
-      console.log(`Latitude : ${crd.latitude}`);
-      // console.log(parseFloat(crd.latitude));
-      // this.lat = parseFloat(crd.latitude);
-      console.log(`Longitude: ${crd.longitude}`);
-      // console.log(parseFloat(crd.longitude));
-      // this.long = parseFloat(crd.longitude);
-      console.log(`More or less ${crd.accuracy} meters.`);
-    }
-
-    function error(err) {
-      console.warn(`ERROR(${err.code}): ${err.message}`);
-    }
-
-    navigator.geolocation.getCurrentPosition(success, error, options);
-
     this.findStoreService
-      .findClosestGamingStoreWithCoords('41.878114','-87.629798')
+      .findClosestGamingStoreWithCoords(lat, long)
       .subscribe(result => {
-        console.log('got it');
-        console.log(result);
-        console.log(result.results[0].address_components[2].long_name);
         this.city = result.results[0].address_components[2].long_name;
-        console.log(result.results[0].address_components[4].short_name);
         this.state = result.results[0].address_components[4].short_name;
         this.yourLocation = this.city + ' , ' + this.state;
         this.destination = "magic the gathering " + this.yourLocation;
@@ -85,9 +87,39 @@ export class FindstoreComponent implements OnInit {
           }
         });
       });
+  }
 
-  } //OnInit
+  getClosestGamingStoreToInputCityAndState(locationInput) {
+        let mapElement = document.getElementById('map');
+        let panelElement = document.getElementById('panel');
+        panelElement.innerHTML = "";  //clear panel so it doesnt append last search directions
 
+        let directionsService = new google.maps.DirectionsService();
+        let directionsDisplay = new google.maps.DirectionsRenderer();
+
+        let map = new google.maps.Map(mapElement, {
+          zoom:7,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        });
+
+        directionsDisplay.setMap(map);
+        directionsDisplay.setPanel(panelElement);
+
+        this.yourLocation = locationInput;
+        this.destination = "magic the gathering " + this.yourLocation;
+
+        let request = {
+          origin: this.yourLocation,
+          destination: this.destination,
+          travelMode: google.maps.TravelMode.DRIVING
+        };
+
+        directionsService.route(request, function(response, status) {
+          if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+          }
+        });
+  }
 
 
 } //end
